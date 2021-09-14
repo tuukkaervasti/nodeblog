3ecf05923d76bad59ba73b4eb24324f28c4940a5
Tuukka Ervasti <tuukka.ervasti@gmail.com>
2021-09-14 17:12:28 +0300

    Refactored code to use .ejs page templates and dynamic loading via Express

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
+// declaring variables and requiring packages
 const express = require('express');
-
+const path = require('path');
 const app = new express();
+const ejs = require('ejs');
 
+app.set("view engine","ejs");
+
+// creating the server
 app.listen(4000, () => {
     console.log("App listening to port 4000");
 });
+
+app.get("/", (req, res) => {
+    res.render('index');
+})
+
+app.get('/about',(req,res)=>{ 
+//res.sendFile(path.resolve(__dirname,'pages/about.html')) 
+    res.render('about');
+}) 
+app.get('/contact',(req,res)=>{
+//res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+res.render('contact');
+}) 
+app.get('/post',(req,res)=>{
+//res.sendFile(path.resolve(__dirname,'pages/post.html'))
+res.render('post')
+})
+
+
 app.use(express.static("public"));
+
+
+
+
+
